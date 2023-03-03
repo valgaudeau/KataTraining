c6c83a0f2313ff2068196ab7f9443552dc1cd636
Jack Linstead <jack.linstead@hotmail.co.uk>
2023-03-03 16:10:09 +0000

    Ohce Solution

---
+++ ConsoleApp1/Ohce/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string output = Console.ReadLine();
+            while (output != "stop")
+            {
+                Console.WriteLine("What's Your Name?");
+                string name = Console.ReadLine();
+            }
+
+        }
+        public static void Greeting(string name)
+        {
+
+            DateTime now = new DateTime();
+            int hour = now.Hour;
+            if (hour < 20 && hour > 6)
+            {
+                Console.WriteLine($" ¡Buenas noches {name}");
+            }
+            else if (hour < 6 && hour > 12)
+            {
+                Console.WriteLine($" ¡Buenas dias {name}");
+            }
+            else if (hour < 12 && hour > 20)
+            {
+                Console.WriteLine($" ¡Buenas target {name}");
+            }
+        }
+
+    }
+}
